7a0e03a524818255ecf72227aa0ea34e695e7164
Chris Woodcock <chris@humesoftware.com>
2024-09-17 15:19:28 +0100

    Removed unused code

--- src/SFA.DAS.RequestApprenticeTraining.Domain/Types/RequestType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SFA.DAS.RequestApprenticeTraining.Domain.Types
-{
-    public enum RequestType : byte
-    {
-        Shortlist = 0,
-        CourseDetail = 1,
-        Providers = 2
-    }
-}
